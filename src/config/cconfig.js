29016afbf3e08e023b45e1093335e412e8879358
yamdeng <yamdeng@gmail.com>
2020-10-18 11:21:17 +0900

    작업중

--- src/config/cconfig.js
+++ src/config/cconfig.js
@@ -12,4 +12,13 @@ Config.LOG_MAX_FILE_SIZE = 10485760;
 // 로그파일 rolling 기준 파일 갯수
 Config.LOG_MAX_FILE_COUNT = 3;
 
+// db connection info
+Config.db = {
+  host: 'localhost',
+  user: 'scglab2',
+  password: '1234',
+  port: 3306,
+  database: 'sdtprototype'
+};
+
 module.exports = Config;
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,5 +1,37 @@
 'use strict';
+const _ = require('lodash');
 
-const Helper = {};
+// isEmp ---> is_emp
+const changeKeyToUnderScore = function (paramObject) {
+  let convertParam = {};
+  let paramKeys = _.keys(paramObject);
+  paramKeys.forEach((paramKey) => {
+    convertParam[_.snakeCase(paramKey)] = paramObject[paramKey];
+  });
+  return convertParam;
+};
 
-module.exports = Helper;
+// is_emp ---> isEmp
+const changeKeyToCamelCase = function (paramObject) {
+  let convertParam = {};
+  let paramKeys = _.keys(paramObject);
+  paramKeys.forEach((paramKey) => {
+    convertParam[_.camelCase(paramKey)] = paramObject[paramKey];
+  });
+  return convertParam;
+};
+
+const changeResultKeyToUnderScore = function (dbResults) {
+  let convertResults = [];
+  if (dbResults && dbResults.length) {
+    dbResults.forEach((dbInfo) => {
+      convertResults.push(changeKeyToCamelCase(dbInfo));
+    });
+  }
+  return convertResults;
+};
+
+module.exports = {
+  changeKeyToUnderScore: changeKeyToUnderScore,
+  changeResultKeyToUnderScore: changeResultKeyToUnderScore
+};
