80a9f629bde01ba0fbdb8378ce3bf9a77bb26b45
chiseong-ahn <69830371+chiseong-ahn@users.noreply.github.com>
2021-03-09 11:31:11 +0900

    Update ddl.sql
    room.room_state_IDX 인덱스 추가.

--- doc/ddl.sql
+++ doc/ddl.sql
@@ -606,6 +606,8 @@ update room
 set join_message_id = null
 where join_message_id = 0;
 
+CREATE INDEX room_state_IDX USING BTREE ON room (state);
+
 -- room_join_history
 INSERT INTO room_join_history (id, create_date, update_date, company_id, member_id, room_id, start_message_id, end_message_id, end_date, last_member_id, category_small_id, join_history_json)
 SELECT id, createdate, workdate, CONCAT(cid, ''), emp, space, startid, endid, enddt, lastemp, catesm, prehistory
